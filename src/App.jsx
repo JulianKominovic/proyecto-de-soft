bad6111ae2dfdb028d892a5ebe319965106ee909
cimi88 <94465130+cimi88@users.noreply.github.com>
2022-05-26 01:41:22 -0300

    app-css-arreglo
    app-css-arreglo

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,12 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <section //logo mcdonald's
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          padding: "5px 0",
-          position: "fixed",
-          background: "#d8070f",
-          width: "100%",
-          boxShadow: "0 -5px 5px 6px rgba(0,0,0,.5)",
-          zIndex:"3",
-        }}
-      >
-        <img
+      {/* logo mcdonald's */}
+      <section className="logo-container">
+        <img className="img-logo"
           src="https://upload.wikimedia.org/wikipedia/commons/3/36/McDonald%27s_Golden_Arches.svg"
           alt="mc-logo"
           title="Me Encanta"
-          width={"30px"}
         />
       </section>
       <BrowserRouter>
--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -68,6 +68,7 @@ const Catalogo = () => {
           />
         ))}
       </section>
+      
     </>
   );
 };
