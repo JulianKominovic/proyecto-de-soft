7b7ed3a6d24d7189de215e51880fdde84980a702
eze <salgueiroezequieli@gmail.com>
2022-05-28 14:01:41 -0300

    pagina carrito

--- src/components/CardProducto/CardProducto.jsx
+++ src/components/CardProducto/CardProducto.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import "./CardProducto.css";
 
-const CardProducto = ({ imagen, precio, descripcion }) => {
+const CardProducto = ({ imagen, precio, descripcion, unidades}) => {
   return (
     <Link
       to={{ pathname: "/detalles", search: "id=62965f37a9f892b42b50e5ec" }}
@@ -10,7 +10,9 @@ const CardProducto = ({ imagen, precio, descripcion }) => {
       <div className="card-precio">
         <p>{"$" + (precio - 1)}</p>
       </div>
+
       <img className="card-img" src={imagen} />
+
       <p className="card-descripcion">{descripcion}</p>
     </Link>
   );
