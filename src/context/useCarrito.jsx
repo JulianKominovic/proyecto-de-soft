cb8e94cbf8c4b5aad4cb9f97ed6f0876385fc253
JulianKominovic <juliankominovic@gmail.com>
2022-06-14 19:34:55 -0300

    Persitance done

--- src/context/useCarrito.jsx
+++ src/context/useCarrito.jsx
@@ -3,48 +3,45 @@ import { useContext } from "react";
 import context from "./Context";
 
 const useCarrito = () => {
-	const { carritoItems, setCarritoItems } = useContext(context);
+  const { carritoItems, setCarritoItems } = useContext(context);
 
-	/**
-	 * Agregar producto (agregar el objeto)
-	 * @param {object} objeto
-	 */
-	const addItem = (newItem, unidades) => {
-		setCarritoItems((prev) => [
-			...prev,
-			{ ...newItem, unidades: unidades },
-		]);
-	};
+  /**
+   * Agregar producto (agregar el objeto)
+   * @param {object} objeto
+   */
+  const addItem = (newItem, unidades) => {
+    setCarritoItems((prev) => [...prev, { ...newItem, unidades: unidades }]);
+  };
 
-	/**
-	 * Eliminar item por ID
-	 * @param {string} id
-	 */
-	const removeItem = (id = "") => {
-		setCarritoItems((prev) => prev.filter((item) => item._id !== id));
-	};
+  /**
+   * Eliminar item por ID
+   * @param {string} id
+   */
+  const removeItem = (id = "") => {
+    setCarritoItems((prev) => prev.filter((item) => item._id !== id));
+  };
 
-	/**
-	 * Traer los items del carrito
-	 */
+  /**
+   * Traer los items del carrito
+   */
 
-	const getCarritoItems = () => carritoItems;
+  const getCarritoItems = () => carritoItems || [];
 
-	const cambiarCantidadItem = (id, cantidad) => {
-		setCarritoItems((prev) =>
-			prev.map((item) => {
-				if (item._id === id) item.unidades = cantidad;
-				return item;
-			})
-		);
-	};
+  const cambiarCantidadItem = (id, cantidad) => {
+    setCarritoItems((prev) =>
+      prev.map((item) => {
+        if (item._id === id) item.unidades = cantidad;
+        return item;
+      })
+    );
+  };
 
-	return {
-		addItem,
-		removeItem,
-		getCarritoItems,
-		cambiarCantidadItem,
-	};
+  return {
+    addItem,
+    removeItem,
+    getCarritoItems,
+    cambiarCantidadItem,
+  };
 };
 
 export default useCarrito;
--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -13,52 +13,50 @@ const Carrito = () => {
   const { getCarritoItems } = useCarrito();
 
   const precioTotal = getCarritoItems().reduce(
-		(total, currentValue) =>
-			(total = total + currentValue.unidades * currentValue.price),
-		0
+    (total, currentValue) =>
+      (total = total + currentValue.unidades * currentValue.price),
+    0
   );
 
   return (
-		<>
-			<section className="lista-carrito">
-				<header className="carrito__header">
-					<Link
-						className="boton-link"
-						to={"/catalogo"}
-						key={"Volver"}
-					>
-						<FlechaAtras style={{ color: "black" }} />
-					</Link>
-					<h1 className="carrito_title">Carrito</h1>
-				</header>
+    <>
+      <section className="lista-carrito">
+        <header className="carrito__header">
+          <Link className="boton-link" to={"/catalogo"} key={"Volver"}>
+            <FlechaAtras style={{ color: "black" }} />
+          </Link>
+          <h1 className="carrito_title">Carrito</h1>
+        </header>
 
-				{getCarritoItems().map((combo) => (
-					<CarritoCard
-						key={combo._id}
-						nombre={combo.name}
-						img={combo.image}
-						precio={combo.price}
-						desc={combo.description}
-						unidades={combo.unidades}
-						id={combo._id}
-					/>
-				))}
-			</section>
+        <main className="carrito__cards__main">
+          {getCarritoItems().map((combo) => (
+            <CarritoCard
+              key={combo._id}
+              nombre={combo.name}
+              img={combo.image}
+              precio={combo.price}
+              desc={combo.description}
+              unidades={combo.unidades}
+              id={combo._id}
+            />
+          ))}
+        </main>
+      </section>
 
-			<section className="precio-total">
-				<p>Total</p>
-				<p>${precioTotal}</p>
-			</section>
+      <section className="precio-total">
+        <p>Total</p>
+        <p>${precioTotal}</p>
+      </section>
 
-			<div className="botones-inferiores">
-				<Link to="/catalogo">
-					<button className="boton-volver">Seguir pidiendo</button>
-				</Link>
-				<Link to="/pago">
-					<button className="boton-pagar">Pagar</button>
-				</Link>
-			</div>
-		</>
+      <div className="botones-inferiores">
+        <Link to="/catalogo">
+          <button className="boton-volver">Seguir pidiendo</button>
+        </Link>
+        <Link to="/pago">
+          <button className="boton-pagar">Pagar</button>
+        </Link>
+      </div>
+    </>
   );
 };
 
